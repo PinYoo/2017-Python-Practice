ff8a03a3f2b05dce10e38b9ba5a2e6f1f3da6b9f
LiangWei <pinyo9611@gmail.com>
2017-02-04 23:27:25 +0800

    Homework done.

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import sys
 from datetime import datetime
 
 import tensorflow as tf
+import cv2
 
 '''
 1. import `Video` and `save_video` from the correct module of package "styler"
 '''
-from ... import Video
-from ... import save_video
+from styler.video import Video
+from styler.utils import save_video
 
 
 model_file = 'data/vg-30.pb'
@@ -41,7 +42,7 @@ def main():
         '''
         2. set the `path` to  your input
         '''
-        with Video(...) as v:
+        with Video("input/corn.mp4") as v:
             frames = v.read_frames(image_h=shape[1], image_w=shape[2])
 
         logging.info("Processing image")
@@ -51,17 +52,20 @@ def main():
         3. Write a list comprehension to iterate through all frames,
            and make it be processed by Tensorflow.
         '''
-        processed = [
-            session.run(out, feed_dict={image: [frame]})
-            ...
-        ]
+        num = 1
+        processed = []
+        for frame in frames:
+            logging.info("Processing frame %d" % num)
+            num += 1
+            output = session.run(out, feed_dict={image: [frame]})
+            processed.append(output)
 
         '''
         4. Pass the results as a argument into function
         '''
         save_video('result.mp4',
                    fps=30, h=shape[1], w=shape[2],
-                   frames=...)
+                   frames=processed)
 
         logging.info("Processing took %f" % (
             (datetime.now() - start_time).total_seconds()))
